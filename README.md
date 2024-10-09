2102fd80f21966ba335a560865e69f0c18e9da8d
dianyasm <dianyasm@gmail.com>
2024-10-09 10:52:28 +0200

    Esquema

---
+++ README.md
@@ -0,0 +1,24 @@
+# Cumpleaños
+
+##  Descripcion
+Ejercicio de una Aplicacion web que al seleccionar tu fecha de nacimiento, te devuelve los dias que faltan para tu cumpleaños.
+
+## Tecnologías
+- html
+- css
+- js (vanilla)
+- npm 
+- vite
+
+## Dependencias
+- gh-pages
+  
+## Deploy en GitHub
+- https://dianyasm.github.io/saludator/
+
+## Sprint 1
+
+- [x] Crear la estructura html
+- [x] Aplicar estilos con css
+- [x] Dotar de comportamiento con js
+- [x] Deploy
---
+++ main.js
@@ -0,0 +1,21 @@
+import './style.css'
+
+document.getElementById('fecha').addEventListener('change', calcularDias);
+
+
+function calcularDias() {
+  const cumpleFecha = document.getElementById('fecha').value;
+
+  const hoy = new Date();
+  const cumple = new Date(cumpleFecha);
+
+  cumple.setFullYear(hoy.getFullYear());
+
+  if (cumple < hoy) {
+    cumple.setFullYear(hoy.getFullYear() + 1);
+  }
+
+  const dias = Math.floor((cumple - hoy) / (1000 * 60 * 60 * 24))+1
+
+  document.getElementById('resp').innerText = `Faltan ${dias} dias para tu cumpleaños.`;
+}
